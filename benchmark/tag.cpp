a3c8cba765fea88f6f9d22f110df0c84dc683105
Georgii Ekserdzhian <gxrjan@gmail.com>
2021-03-04 10:02:14 +0100

    tag.cpp now uses 16 bit chars

--- benchmark/tag.cpp
+++ benchmark/tag.cpp
@@ -2,14 +2,14 @@
 #include <iostream>
 using namespace std;
 
-static string rules[] = { "bc", "a", "aaa"};
+static u16string rules[] = { u"bc", u"a", u"aaa"};
 
 
-static int iter(string s) {
+static int iter(u16string s) {
     int count = 0;
     
-    while (s != "a") {
-        s = s.substr(2) + rules[s[0] - 'a'];
+    while (s != u16string(u"a")) {
+        s = s.substr(2) + rules[s[0] - u'a'];
         count += 1;
     }
     return count;
@@ -19,6 +19,6 @@ int main(int argc, char *argv[]) {
     int n = stoi(argv[1]);
     int total = 0;
     for (int i = 1 ; i <= n ; ++i)
-        total += iter(string(i , 'a'));
+        total += iter(u16string(i , u'a'));
     cout << "the total is: " << total << endl;
 }

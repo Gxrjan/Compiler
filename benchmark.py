032fcf62839a06876dc770516dc96708a827e561
Georgii Ekserdzhian <gxrjan@gmail.com>
2021-03-31 09:17:53 +0200

    Changed the command line arguments on some benchmarks because they were taking too long

--- benchmark.py
+++ benchmark.py
@@ -5,26 +5,26 @@ from helpers import *
 def main():
     print("Running benchmarks...")
 
-    # Merge sort 10000000 elements, 100 - random seed
-    do_test("Merge sort", 'merge_sort', [10000000, 100])
+    # # Merge sort 10000000 elements, 100 - random seed
+    # do_test("Merge sort", 'merge_sort', [10000000, 100])
 
-    # Insertion sort 100000 elements, 100 - random seed
-    do_test("Insertion sort", 'insertion_sort', [100000, 100])
+    # # Insertion sort 100000 elements, 100 - random seed
+    # do_test("Insertion sort", 'insertion_sort', [100000, 100])
 
     # prime sum 20000 elements
-    do_test("Prime sum", 'prime_sum', [20000])
+    do_test("Prime sum", 'prime_sum', [10000])
 
-    # Tag 300 elements
-    do_test("Tag", 'tag', [300])
+    # # Tag 300 elements
+    # do_test("Tag", 'tag', [300])
 
-    # String permutations on ABCDEFGHIJ
-    do_test("String permutations", 'perm', ["ABCDEFGHIJ"])
+    # # String permutations on ABCDEFGHIJ
+    # do_test("String permutations", 'perm', ["ABCDEFGHIJ"])
 
-    # Prime count on 1000000000
-    do_test("Prime count", 'prime_count', [1000000000])
+    # # Prime count on 100000000
+    # do_test("Prime count", 'prime_count', [100000000])
 
-    # Hash table on 100000
-    do_test("Hash table", 'hash_table', [1000000, 13])
+    # # Hash table on 100000
+    # do_test("Hash table", 'hash_table', [1000000, 13])
 
 
 
